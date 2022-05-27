5bcdf50f987a97e540e9c7c39b00b024a5e3fdf9
ROD <104014121+prorodrigoh@users.noreply.github.com>
2022-05-26 22:01:24 -0400

    added function to updaterating

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ import functions from 'firebase-functions';
 import express from 'express';
 import cors from 'cors';
 import { getAllRestaurants, getRestaurantById, deleteRestaurant,
-  updateRestaurant, addRestaurant } from './src/restaurants.js';
+  updateRestaurant, updateRating, addRestaurant } from './src/restaurants.js';
 
 const app = express();
 app.use(cors());
@@ -13,6 +13,7 @@ app.get('/restaurants', getAllRestaurants);
 app.get('/restaurants/:restaurantId', getRestaurantById);
 app.delete('/restaurants/:restaurantId', deleteRestaurant);
 app.patch('/restaurants/:restaurantId', updateRestaurant);
+app.patch('/restaurants/:restaurantId/rating', updateRating);
 app.post('/restaurants', addRestaurant);
 
 // the function in the firebase.json
--- functions/src/restaurants.js
+++ functions/src/restaurants.js
@@ -67,6 +67,38 @@ export function updateRestaurant(req, res) {
     });
 }
 
+
+export function updateRating(req, res) {
+  if(!req.params || !req.params.restaurantId || req.body.rating > 5 || req.body.rating < 0) {
+    res.status(401).send('Invalid request');
+    return;
+  }
+  const { restaurantId } = req.params;
+  let newRating = req.body.rating
+
+  const db = connectDb();
+
+  db.collection('restaurants').doc(restaurantId).get()
+    .then(doc => {
+      // get rating history from DB
+      const {ratingHistory} = doc.data()
+      // add new
+      const newRatingHistory = (ratingHistory) ? [...ratingHistory, newRating] : [newRating]
+      // calculate new average
+      const numRatings = newRatingHistory.length
+      const rating = newRatingHistory.reduce((accum, elem) => accum + elem, 0)/ numRatings
+      // create object with new values
+      const updatedData = { ratingHistory: newRatingHistory, numRatings, rating }
+      // update the database & return the promise to the next .then
+      return db.collection('restaurants').doc(restaurantId).update(updatedData)
+    })
+    .then(() => getRestaurantById(req, res)
+    )    
+    .catch(err => {
+      res.status(500).send(err);
+    });
+}
+
 export function deleteRestaurant(req, res) {
   const { restaurantId } = req.params;
   if(!restaurantId) {
